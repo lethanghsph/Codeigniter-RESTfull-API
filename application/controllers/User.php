fdb6e00763500847b44084afcc6b79e8ef2450ed
lethang <lethanghsph@gmail.com>
2019-03-22 11:48:53 +0700

    Add CRUD resource

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,8 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends MY_Controller
 {
-    public function show()
+    /**
+     * Display a listing of resource.
+     */
+    public function index() {}
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store() {}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     */
+    public function show($id)
     {
-        $this->responseItem(['title' => 'Big title']);
+        return $this->responseItem(['title' => 'Big title']);
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     */
+    public function update($id) {}
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param $id
+     */
+    public function destroy($id) {}
 }
